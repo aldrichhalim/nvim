b6524b766cd1147e4a3d217afaac4fa8de81a195
Aldrich Halim <aldrichvhalim@gmail.com>
2025-07-26 20:36:13 +0700

    refactor: optimize options
    Signed-off-by: Aldrich Halim <aldrichvhalim@gmail.com>

--- init.lua
+++ init.lua
@@ -295,9 +295,7 @@ require('lazy').setup {
         { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'Help tags' },
       },
       dependencies = { 'nvim-lua/plenary.nvim' },
-      config = function()
-        require('telescope').setup {}
-      end,
+      opts = {},
     },
 
     {
